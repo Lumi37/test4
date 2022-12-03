db049dfb356f6223eb74458fd82f136ced61fff1
Lumi <37skmg@gmail.com>
2022-12-03 16:54:41 +0200

    first commit

---
+++ src/client/index.js
@@ -0,0 +1,4 @@
+let fieldUsername = document.querySelector("input[type='text']")
+let fieldPassword = document.querySelector("input[type='password']")
+let saveButton = document.querySelector("#save")
+let loadButton = document.querySelector("#load")
