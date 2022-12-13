ae0ea4a16da815ed6dce74c00aafefac0c31b698
kostas <eoppep@eoppep.gr>
2022-12-13 10:18:05 +0200

    goal reached

--- src/client/index.js
+++ src/client/index.js
@@ -1,26 +1,31 @@
-
+const loadedWindowResponse = await fetch('/loadwindow')
+const responseJSON = await loadedWindowResponse.json()
 
 let fieldUsername = document.querySelector("input[type='text']")
 let fieldPassword = document.querySelector("input[type='password']")
 let saveButton = document.querySelector("#save")
 let loadButton = document.querySelector("#load")
+fieldUsername.value = responseJSON.logInformation.username;
+fieldPassword.value = responseJSON.logInformation.password;
 
 saveButton.addEventListener("click", e => {
     let username = fieldUsername.value;
     let password = fieldPassword.value;
+    console.log(username , password)
     const logInfo = { username, password }
     const requestInfo = {
         method: 'POST',
-        headers: { 'Content-type': 'plain/text' },
+        headers: { 'Content-type': 'application/json' },
         body: JSON.stringify(logInfo)
     }
-    fetch('/pr', logInfo);
+    fetch('/pr', requestInfo);
 })
 loadButton.addEventListener("click", async e => {
     const response = await fetch('/gr')
     const json = await response.json()
-    //  console.log("")
-    fieldUsername = json.username;
-    fieldPassword = json.passwordl
+    console.log("GET --- ",json.logInformation.username,'--------------',json.logInformation.password)
+    fieldUsername.value = json.logInformation.username;
+    fieldPassword.value = json.logInformation.password;
+
 
 })
--- src/server/server.js
+++ src/server/server.js
@@ -3,29 +3,80 @@ import fs from 'fs/promises'
 
 const serverApp = express();
 
-serverApp.use(express.static('src/client'));
+serverApp.use(express.static('../client'));
 serverApp.use(express.json());
 
 serverApp.post('/pr', async (req, res) => {
-    let usernameData = req.body.usernameData
-    let passwordData = req.body.passwordData
-    try {
-        await fs.writeFile('/home/lumi/test4/src/server/username.txt', usernameData);
-    } catch (err) {
-        console.log(err);
+    console.log (await req.body.username, '----', await req.body.password )
+    async function writeUsername(){
+        try{
+            await fs.writeFile('/home/kostas/test4/src/server/username.txt', req.body.username)
+        }catch (err){
+            console.log(err)
+        }
     }
-    try {
-        await fs.writeFile('/home/lumi/test4/src/server/password.txt', passwordData);
-    } catch (err) {
-        console.log(err);
+    async function writePassword(){
+        try{
+            await fs.writeFile('/home/kostas/test4/src/server/password.txt', req.body.password)
+        }catch (err){
+            console.log(err)
+        }
     }
+    writeUsername()
+    writePassword()
 
     res.end();
 
 })
 
-serverApp.get('/pr', (req, res) => {
+serverApp.get('/gr', async (req, res) => {
+    let logInformation = {}
+    async function readUsername(){
+        try{
+            logInformation.username = await fs.readFile('/home/kostas/test4/src/server/username.txt', { encoding: 'utf8' })
+            console.log('U',logInformation.username)
+        }catch (err){
+            console.log(err)
+        }
+    }
+    async function readPassword(){
+        try{
+            logInformation.password = await fs.readFile('/home/kostas/test4/src/server/password.txt', { encoding: 'utf8' })
+            console.log('P',logInformation.password)
+        }catch (err){
+            console.log(err)
+        }
+    }
+    await readUsername()
+    await readPassword()
+    res.json({logInformation})
+    res.end()
 
 })
 
-serverApp.listen(3000, "listens to 3000");
+serverApp.get('/loadwindow', async (req,res)=>{
+    let logInformation = {}
+    async function readUsername(){
+        try{
+            logInformation.username = await fs.readFile('/home/kostas/test4/src/server/username.txt', { encoding: 'utf8' })
+            console.log('U',logInformation.username)
+        }catch (err){
+            console.log(err)
+        }
+    }
+    async function readPassword(){
+        try{
+            logInformation.password = await fs.readFile('/home/kostas/test4/src/server/password.txt', { encoding: 'utf8' })
+            console.log('P',logInformation.password)
+        }catch (err){
+            console.log(err)
+        }
+    }
+    await readUsername()
+    await readPassword()
+    res.json({logInformation})
+    res.end()
+
+})
+
+serverApp.listen(3000, console.log("listens to 3000"));
